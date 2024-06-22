ce7320e72f1a4f1359ba110f1bf8fa922f96a0a0
dtrosien <dtrosien@gmail.com>
2024-06-22 16:14:25 +0200

    implementation of physical plan

--- src/physical_plan/selection_exec.rs
+++ src/physical_plan/selection_exec.rs
@@ -1,3 +1,4 @@
-// todo implement and start writing query planner to check if physical plan works like intended
+// todo implement
+// todo start writing query planner to check if physical plan works like intended
 // todo think about how to create test RecordBatches for Expression tests
 // todo think about how to implement Expressions hierarchy
